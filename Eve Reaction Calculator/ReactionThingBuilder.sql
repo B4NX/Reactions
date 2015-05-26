2a5a9d3f8005c09a6ba03eb87598c74dcf78c50a
Nikko <nikko.rush@gmail.com>
2015-05-25 22:31:45 -0700

    Script changes

--- Eve Reaction Calculator/ReactionThingBuilder.sql
+++ Eve Reaction Calculator/ReactionThingBuilder.sql
@@ -1,6 +1,10 @@
-SELECT reactionTypeID,typeID,quantity FROM Mosaic.dbo.invTypeReactions WHERE input = 1;
+SELECT ITEMS.typeName, ITEMS.typeID FROM Mosaic.dbo.invTypeReactions AS REACTIONS, Mosaic.dbo.invTypes AS ITEMS WHERE REACTIONS.reactionTypeID = ITEMS.typeID;
 
---SELECT ITEMS.typeID, ITEMS.typeName FROM Mosaic.dbo.invTypeReactions AS REACTIONS, Mosaic.dbo.invTypes AS ITEMS WHERE REACTIONS.reactionTypeID = ITEMS.typeID;
 
-(SELECT Items.typeID,typeName FROM Mosaic.dbo.invTypes AS Items INNER JOIN Mosaic.dbo.invTypeReactions AS Reactions ON Reactions.reactionTypeID = Items.typeID)
+--SELECT Items.typeName FROM Mosaic.dbo.invTypeReactions AS Reactions, Mosaic.dbo.invTypes AS Items WHERE Reactions.reactionTypeID = Items.typeID
 
+--SELECT Item.typeName, Reaction.typeID FROM Mosaic.dbo.invTypeReactions, Mosaic.dbo.invTypes AS Reaction WHERE Reaction.input = 1
+
+--SELECT Items.typeName FROM Mosaic.dbo.invTypeReactions AS Reactions, Mosaic.dbo.invTypes AS Items WHERE Reactions.reactionTypeID = Items.typeID
+--UNION ALL
+--SELECT Item.typeName FROM Mosaic.dbo.invTypeReactions AS Reaction, Mosaic.dbo.invTypes AS Item WHERE Reaction.input= 1;
